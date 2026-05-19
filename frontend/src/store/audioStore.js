c7aec3ac3eefa38fac3d3b7f7629905ab6aba494
2024ci Aayushkumar <59455057-2024ciaayushkum@users.noreply.replit.com>
2026-05-19 09:33:55 +0530

    feat: add frontend/src/store/audioStore.js

---
+++ frontend/src/store/audioStore.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+export const useAudioStore = create((set, get) => ({
+  currentJob: null,
+  uploadProgress: 0,
+  isUploading: false,
+  isAnalyzing: false,
+  history: [],
+  historyPage: 1,
+  historyTotal: 0,
+
+  setCurrentJob: (job) => set({ currentJob: job }),
+  setUploadProgress: (progress) => set({ uploadProgress: progress }),
+  setIsUploading: (val) => set({ isUploading: val }),
+  setIsAnalyzing: (val) => set({ isAnalyzing: val }),
+  setHistory: (history, total) => set({ history, historyTotal: total }),
+  resetUpload: () => set({ currentJob: null, uploadProgress: 0, isUploading: false, isAnalyzing: false }),
+}));
